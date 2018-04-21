a65818181df2ebce5681093bb5da825d7913c3e3
chivitc1 <chivitc1@gmail.com>
2018-04-21 11:27:54 +0700

    Add JPA entity to domain model: domain model === data model

--- src/main/java/com/example/multiplication/domain/MultiplicationResultAttempt.java
+++ src/main/java/com/example/multiplication/domain/MultiplicationResultAttempt.java
@@ -5,6 +5,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
 /**
  * Identifies the attempt from a {@link User} to solve a
  * {@link Multiplication}.
@@ -13,18 +20,31 @@ import lombok.ToString;
 @EqualsAndHashCode
 @ToString
 @RequiredArgsConstructor
+@Entity
 public final class MultiplicationResultAttempt
 {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "USER_ID")
 	private final User user;
+
+	@ManyToOne(cascade = CascadeType.PERSIST)
+	@JoinColumn(name = "MULTIPLICATION_ID")
 	private final Multiplication multiplication;
+
 	private final int resultAttempt;
 
 	private final boolean correct;
 
-	MultiplicationResultAttempt() {
+	MultiplicationResultAttempt()
+	{
 		user = null;
 		multiplication = null;
 		resultAttempt = -1;
 		correct = false;
 	}
+
 }
--- src/main/java/com/example/multiplication/domain/User.java
+++ src/main/java/com/example/multiplication/domain/User.java
@@ -5,6 +5,11 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
 /**
  * Store information to identify the user
  */
@@ -12,8 +17,14 @@ import lombok.ToString;
 @RequiredArgsConstructor
 @ToString
 @EqualsAndHashCode
+@Entity
 public final class User
 {
+	@Id
+	@GeneratedValue
+	@Column(name = "MULTIPLICATION_ID")
+	private Long id;
+
 	private final String alias;
 
 	protected User() {
