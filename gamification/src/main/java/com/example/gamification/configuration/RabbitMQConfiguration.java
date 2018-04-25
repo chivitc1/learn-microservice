081408ed7dd680ef98aa6423ab415ae9728be0a3
chivitc1 <chivitc1@gmail.com>
2018-04-25 17:41:25 +0700

    Gamification: setup RabbitMQ config for handling incoming message

---
+++ gamification/src/main/java/com/example/gamification/configuration/RabbitMQConfiguration.java
@@ -0,0 +1,70 @@
+package com.example.gamification.configuration;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.annotation.RabbitListenerConfigurer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpoint;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistrar;
+import org.springframework.amqp.rabbit.listener.RabbitListenerEndpointRegistry;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.handler.annotation.support.DefaultMessageHandlerMethodFactory;
+import org.springframework.messaging.converter.MappingJackson2MessageConverter;
+
+@Configuration
+public class RabbitMQConfiguration implements RabbitListenerConfigurer
+{
+	@Bean
+	public TopicExchange multiplicationExchange(
+			@Value("${multiplication.exchange}") final String _exchangeName
+	)
+	{
+		return new TopicExchange(_exchangeName);
+	}
+
+	@Bean
+	public Queue gamificationMultiplicationQueue(
+			@Value("${multiplication.queue}") final String _queueName
+	)
+	{
+		return new Queue(_queueName, true);
+	}
+
+	@Bean
+	public Binding binding(final Queue _queue, final TopicExchange _exchange,
+		   @Value("${multiplication.anything.routing-key}") final String _routingKey) {
+		return BindingBuilder.bind(_queue).to(_exchange).with(_routingKey);
+	}
+
+	@Bean
+	public MappingJackson2MessageConverter messageConverter() {
+		return new MappingJackson2MessageConverter();
+	}
+
+	@Bean
+	public DefaultMessageHandlerMethodFactory messageHandlerMethodFactory() {
+		DefaultMessageHandlerMethodFactory factory= new DefaultMessageHandlerMethodFactory();
+		factory.setMessageConverter(messageConverter());
+
+		return factory;
+	}
+
+	/**
+	 * Callback allowing a {@link RabbitListenerEndpointRegistry
+	 * RabbitListenerEndpointRegistry} and specific {@link RabbitListenerEndpoint
+	 * RabbitListenerEndpoint} instances to be registered against the given
+	 * {@link RabbitListenerEndpointRegistrar}. The default
+	 * {@link RabbitListenerContainerFactory RabbitListenerContainerFactory}
+	 * can also be customized.
+	 * @param _registrar the registrar to be configured
+	 */
+	@Override
+	public void configureRabbitListeners(RabbitListenerEndpointRegistrar _registrar)
+	{
+		_registrar.setMessageHandlerMethodFactory(messageHandlerMethodFactory());
+	}
+}
