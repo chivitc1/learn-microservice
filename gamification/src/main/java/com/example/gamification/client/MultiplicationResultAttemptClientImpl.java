54d787a33a9ed374f4ee75c65c4efc3fb3e9a363
chivitc1 <chivitc1@gmail.com>
2018-04-26 06:19:58 +0700

    Gamification: rest client impl

---
+++ gamification/src/main/java/com/example/gamification/client/MultiplicationResultAttemptClientImpl.java
@@ -0,0 +1,36 @@
+package com.example.gamification.client;
+
+import com.example.gamification.client.dto.MultiplicationResultAttempt;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * This implementation of MultiplicationResultAttemptClient
+ interface connects to
+ * the Multiplication microservice via REST.
+ */
+public class MultiplicationResultAttemptClientImpl
+		implements MultiplicationResultAttemptClient
+{
+	private final RestTemplate restTemplate;
+
+	private final String multiplicationHost;
+
+	@Autowired
+	public MultiplicationResultAttemptClientImpl
+			(final RestTemplate restTemplate,
+			 @Value("${multiplicationHost}") final String multiplicationHost) {
+		this.restTemplate = restTemplate;
+		this.multiplicationHost = multiplicationHost;
+	}
+
+	@Override
+	public MultiplicationResultAttempt retrieveMultiplicationResultAttemptbyId(Long multiplicationId)
+	{
+		return restTemplate.getForObject(
+				multiplicationHost + "/results/" + multiplicationId,
+				MultiplicationResultAttempt.class
+		);
+	}
+}
